5441960e00f55b832580e0c8d5994c88c755647b
Alejandro Lopez <alejandro.lopezp@unet.edu.ve>
2022-08-06 16:52:42 -0400

    Add aditional nav bar items
    create new component for nav bar items

--- src/components/nav_menu.jsx
+++ src/components/nav_menu.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MetisMenu from "@metismenu/react";
 import "metismenujs/dist/metismenujs.css";
-import { Link } from "react-router-dom";
+import NavMenuItem from "./widgets/nav_menu_item";
 
 export default function NavMenu(props) {
   const [tabletView, setTabletView] = useState(false);
@@ -26,75 +26,15 @@ export default function NavMenu(props) {
           <div className="col-xl-12">
             <nav className="topbar-nav">
               <MetisMenu className="metismenu" id="metismenu" toggle={true}>
-                <li>
-                  <Link to="/">
-                    <span className="fa fa-home"></span> Inicio
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/calendar">
-                    <span className="fa fa-newspaper-o"></span> Noticias
-                  </Link>
-                </li>
-                <li>
-                  <a className="has-arrow" href="https://www.google.com/">
-                    <span className="fa fa-users "></span> NOSOTROS
-                  </a>
-                  <ul>
-                    <li>
-                      <Link to="/history">Historia</Link>
-                    </li>
-                    <li>
-                      <Link to="/direction_board">Junta directiva</Link>
-                    </li>
-                    <li>
-                      <Link to="/administrative_personal">Personal administrativo</Link>
-                    </li>
-                    <li>
-                      <Link to="/regulations">Reglamentos</Link>
-                    </li>
-                    <li>
-                      <Link to="/statutes">Estatutos</Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="/spaces">
-                    <span className="fa fa-university"></span>
-                    Instalaciones
-                  </Link>
-                </li>
-                <li>
-                  <a className="has-arrow" href="https://www.google.com/">
-                    <span className="fa fa-futbol-o"></span> Escuelas deportivas
-                  </a>
-                  <ul aria-expanded="false">
-                    <li>
-                      <Link to="/message">Karate</Link>
-                    </li>
-                    <li>
-                      <Link to="/chat">Beisbol</Link>
-                    </li>
-                  </ul>
-                </li>
-
-                <li>
-                  <a
-                    className="has-arrow"
-                    href="https://www.google.com/"
-                    aria-expanded="false"
-                  >
-                    <span className="fa fa-cog"></span> Configuracion
-                  </a>
-                  <ul aria-expanded="false">
-                    <li>
-                      <Link to="/profile">Perfil</Link>
-                    </li>
-                    <li>
-                      <Link to="/payment">Deuda</Link>
-                    </li>
-                  </ul>
-                </li>
+                <NavMenuItem title="Inicio" icon="fa fa-home" route="/"></NavMenuItem>
+                <NavMenuItem title="Blog" icon="fa fa-newspaper-o" route="/blog"></NavMenuItem>
+                <NavMenuItem title="Nosotros" icon="fa fa-users" subitems={[{link:"/history",label:"Historia"},{link:"/direction_board",label:"Junta directiva"},{link:"/administrative_personal",label:"Personal administrativo"},{link:"/regulations",label:"Reglamentos"},{link:"/statutes",label:"Estatutos"}]}></NavMenuItem>
+                <NavMenuItem title="Instalaciones" icon="fa fa-university" route="/spaces"></NavMenuItem>
+                <NavMenuItem title="Cultura" icon="fa fa-music " subitems={[{link:"/coral",label:"Coral"}]}></NavMenuItem>
+                <NavMenuItem title="Deportes" icon="fa fa-trophy" subitems={[{link:"/tenis",label:"Tenis"}]} ></NavMenuItem>
+                <NavMenuItem title ="Servicios" icon="fa fa-briefcase" subitems={[{link:"/restaurant",label:"Restaurant"}]}></NavMenuItem>
+                <NavMenuItem title="Escuelas deportivas" icon="fa fa-futbol-o" subitems={[{link:"/soccerSchool",label:"Futbol"}]}/>
+                <NavMenuItem title="Contacto" icon="fa fa-envelope"/>
               </MetisMenu>
             </nav>
           </div>
