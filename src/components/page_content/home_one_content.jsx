221a82a9e149b07a26e601e758d0fcfe12d816ba
Alejandro Lopez <alejandro.lopezp@unet.edu.ve>
2022-08-08 16:13:05 -0400

    add home carousel
    add images for home carousel and modify home item added a service for consume API in news

--- src/components/page_content/home_one_content.jsx
+++ src/components/page_content/home_one_content.jsx
@@ -1,18 +1,40 @@
 import HomeNewsItem from "components/widgets/home_news_item";
 import React from "react";
 import noticia1 from "../../assets/images/news/noticia1.jpg";
+import logo from "../../assets/images/logo2.jpg";
+import HomeCarousel from "components/widgets/home_carousel";
+
 
 export default function HomeOneContent() {
   return (
     <div className="content-body">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="title">
-          <h2>Noticias</h2>
+          <div className="homeLogoDiv">
+            <h1>Centro Latino San Cristobal</h1>
+            <img className="homeLogo img-fluid" width={200} src={logo} />
           </div>
-          <HomeNewsItem label="Jornada de carnetizacion" src={noticia1} alt=""></HomeNewsItem>
-          <HomeNewsItem label="Jornada de carnetizacion" src={noticia1} alt=""></HomeNewsItem>
-          <HomeNewsItem label="Jornada de carnetizacion" src={noticia1} alt=""></HomeNewsItem>
+          <div className="col-xl-12">
+          <HomeCarousel/>
+          </div>
+          <div className="title">
+            <h2>Noticias</h2>
+          </div>
+          <HomeNewsItem
+            label="Jornada de carnetizacion"
+            src={noticia1}
+            alt=""
+          ></HomeNewsItem>
+          <HomeNewsItem
+            label="Jornada de carnetizacion"
+            src={noticia1}
+            alt=""
+          ></HomeNewsItem>
+          <HomeNewsItem
+            label="Jornada de carnetizacion"
+            src={noticia1}
+            alt=""
+          ></HomeNewsItem>
         </div>
       </div>
     </div>
---
+++ src/components/widgets/home_carousel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import carousel1 from "../../assets/images/carousel/1.jpg";
+import carousel2 from "../../assets/images/carousel/2.jpg";
+import carousel3 from "../../assets/images/carousel/3.jpg";
+import carousel4 from "../../assets/images/carousel/4.jpg";
+import carousel5 from "../../assets/images/carousel/5.jpg";
+import { Carousel, CarouselItem } from "react-bootstrap";
+
+const images = [
+  { src: carousel1, alt: "", label:"Bar" },
+  { src: carousel2,alt: "", label:"Beisbol" },
+  { src: carousel3,alt: "", label:"Fiesta" },
+  { src: carousel4,alt: "", label:"Teatro" },
+  { src: carousel5,alt: "", label:"Necesito mejores fotos" },
+];
+
+export default function HomeCarousel() {
+  return (
+    <Carousel className="w-100">
+      {images.map((image, index) => (
+        <CarouselItem key={index}>
+          <img className="d-block carouselCropped" src={image.src} alt={image.alt} />
+          <Carousel.Caption>
+            <h3>{image.label}</h3>
+          </Carousel.Caption>
+        </CarouselItem>
+        
+      ))}
+    </Carousel>
+  );
+}
