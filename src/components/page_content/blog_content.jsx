d0caa4f526c94003ba6456ef36ad817d708a4d1f
joguel63 <80803870+joguel63@users.noreply.github.com>
2022-08-14 15:45:00 -0500

    Fixed warnings

--- src/components/page_content/blog_content.jsx
+++ src/components/page_content/blog_content.jsx
@@ -4,39 +4,32 @@ import NewsDisplay from "components/widgets/news_display";
 import NewsPageNavigation from "components/widgets/news_page_navigator";
 import newImg from "assets/images/logo2.jpg";
 import { useGetNewsService } from "core/hooks/use-get-news-service";
+import Spinner from "components/spinner";
 
 export default function BlogContent() {
   const [news, setNews] = useState([]);
   const { getNewsPage } = useGetNewsService();
-  const newsArray = [];
-  useEffect(() => {
-    getNewsPage(setNews);
-  }, []);
 
-  const getTheShitTogether = () => {
-    return news.map((item) => {
-      const title = item.new.title;
-      const src = item.pictures[0] ?? newImg;
-      newsArray.push({ title: title, src: src });
-    });
-  };
+  useEffect(
+    () => getNewsPage(setNews),
+    //eslint-disable-next-line
+    []
+  );
 
-  getTheShitTogether();
+  if (!news.length) return <Spinner />;
 
   return (
     <div className="content-body">
       <div className="container">
         <NewsCarousel />
         <div className="newsList">
-          {newsArray.map((item, index) => {
-            return (
-              <NewsDisplay
-                key={index}
-                title={item.title}
-                src={item.src.url ?? item.src.url ?? newImg}
-              />
-            );
-          })}
+          {news.map((item, index) => (
+            <NewsDisplay
+              key={index}
+              title={item.new.title ?? ""}
+              src={item?.pictures[0]?.url ?? newImg}
+            />
+          ))}
         </div>
 
         <NewsPageNavigation page="1" />
