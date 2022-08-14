d0f45bea9fe9bbb1fbcc0d4a1e5c588cf3966abc
Alejandro Lopez <alejandro.lopezp@unet.edu.ve>
2022-08-14 16:27:02 -0400

    Add news from API
    add style and content to new page

--- src/components/page_content/blog_content.jsx
+++ src/components/page_content/blog_content.jsx
@@ -1,44 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NewsCarousel from "components/widgets/carousel";
 import NewsDisplay from "components/widgets/news_display";
 import NewsPageNavigation from "components/widgets/news_page_navigator";
-import newImg from "assets/images/news/noticia1.jpg";
+import newImg from "assets/images/logo2.jpg";
 import { useGetNewsService } from "core/hooks/use-get-news-service";
 
 export default function BlogContent() {
+  const [news, setNews] = useState([]);
   const { getNewsPage } = useGetNewsService();
-  useEffect(getNewsPage, []);
-  //TODO: dinamically generate
+  const newsArray = [];
+  useEffect(() => {
+    getNewsPage(setNews);
+  }, []);
+
+  const getTheShitTogether = () => {
+    return news.map((item) => {
+      const title = item.new.title;
+      const src = item.pictures[0] ?? newImg;
+      newsArray.push({ title: title, src: src });
+    });
+  };
+
+  getTheShitTogether();
+
   return (
     <div className="content-body">
       <div className="container">
-        <div className="row">
-          <NewsCarousel />
-          <div className="col-lg">
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-          </div>
-          <div className="col-lg">
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-          </div>
-          <div className="col-lg">
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-            <NewsDisplay title="Titulo de noticia" src={newImg} />
-          </div>
+        <NewsCarousel />
+        <div className="newsList">
+          {newsArray.map((item, index) => {
+            return (
+              <NewsDisplay
+                key={index}
+                title={item.title}
+                src={item.src.url ?? item.src.url ?? newImg}
+              />
+            );
+          })}
         </div>
+
         <NewsPageNavigation page="1" />
       </div>
     </div>
