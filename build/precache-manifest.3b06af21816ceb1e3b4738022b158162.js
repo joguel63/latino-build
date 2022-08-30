a63cf835782e9ecf6781ea24457ba9fc33b14ca6
joguel63 <80803870+joguel63@users.noreply.github.com>
2022-08-30 15:29:33 -0500

    first commit

---
+++ build/precache-manifest.3b06af21816ceb1e3b4738022b158162.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c90603afd2c8846206caa10214d0107",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74411c2e369b73fe9f87",
+    "url": "/static/css/2.24f260d3.chunk.css"
+  },
+  {
+    "revision": "2d341390ff9cee9f86de",
+    "url": "/static/css/main.8e39ccb7.chunk.css"
+  },
+  {
+    "revision": "74411c2e369b73fe9f87",
+    "url": "/static/js/2.3e6fad97.chunk.js"
+  },
+  {
+    "revision": "1de2038d91275738c2567026727ba42e",
+    "url": "/static/js/2.3e6fad97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d341390ff9cee9f86de",
+    "url": "/static/js/main.b214436b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b214436b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47679af4b622fbe61a7a",
+    "url": "/static/js/runtime-main.17b8782d.js"
+  },
+  {
+    "revision": "cae4642e191ef532224a91e956f10f93",
+    "url": "/static/media/1.cae4642e.jpg"
+  },
+  {
+    "revision": "28a65efec6229ba1930d867942b6a3fc",
+    "url": "/static/media/2.28a65efe.jpg"
+  },
+  {
+    "revision": "c43187fb35d7be5e8d5e02d0c862adce",
+    "url": "/static/media/3.c43187fb.jpg"
+  },
+  {
+    "revision": "47b1d9697efba2d6855bf5f7ac0b32e8",
+    "url": "/static/media/4.47b1d969.jpg"
+  },
+  {
+    "revision": "1fd1840a235ac71c8d07e347c9c0ba17",
+    "url": "/static/media/5.1fd1840a.jpg"
+  },
+  {
+    "revision": "11c4d4aa76e950115ff4a2dab838320c",
+    "url": "/static/media/9.11c4d4aa.jpg"
+  },
+  {
+    "revision": "5ca6bb52f48d48cbaf0cf2b0a5b20d8d",
+    "url": "/static/media/balls.5ca6bb52.jpg"
+  },
+  {
+    "revision": "dc6dfd7c553a9caf469413e39fdc9867",
+    "url": "/static/media/bar.dc6dfd7c.jpeg"
+  },
+  {
+    "revision": "e6894da2af9b02424bf963420aee7276",
+    "url": "/static/media/bar.e6894da2.jpg"
+  },
+  {
+    "revision": "173d75fe85f0b0c19456a04543bd74c4",
+    "url": "/static/media/base.173d75fe.jpg"
+  },
+  {
+    "revision": "8ad6fa913c1b9ec34d8387e9699d87dd",
+    "url": "/static/media/basquet.8ad6fa91.jpg"
+  },
+  {
+    "revision": "28a65efec6229ba1930d867942b6a3fc",
+    "url": "/static/media/beisbol.28a65efe.jpeg"
+  },
+  {
+    "revision": "a9ea0eb40528a28317c7fa3b3d2f8e7b",
+    "url": "/static/media/bohio.a9ea0eb4.jpg"
+  },
+  {
+    "revision": "2c7409728c92502f8efc4babb00d3977",
+    "url": "/static/media/bolas-criollas.2c740972.jpg"
+  },
+  {
+    "revision": "af07c4a8fdff9d94f3521e9d30741a37",
+    "url": "/static/media/cancha-tenis.af07c4a8.jpg"
+  },
+  {
+    "revision": "47b1d9697efba2d6855bf5f7ac0b32e8",
+    "url": "/static/media/conference.47b1d969.jpg"
+  },
+  {
+    "revision": "67c9122da4a4a98dd2c863ddc607bf52",
+    "url": "/static/media/conferencias.67c9122d.jpg"
+  },
+  {
+    "revision": "b3b5657b162443f1eb94c0e9ef3a33fd",
+    "url": "/static/media/coral.b3b5657b.jpeg"
+  },
+  {
+    "revision": "fb22dcc2c5765c4f864120bd8f63fff1",
+    "url": "/static/media/danza.fb22dcc2.jpg"
+  },
+  {
+    "revision": "df760143733f810e7bc26ebaa5754e42",
+    "url": "/static/media/estacionamiento.df760143.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont3e6e.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "fc40de36b106b5030e8b0044be7983e3",
+    "url": "/static/media/futbol-sala.fc40de36.png"
+  },
+  {
+    "revision": "33518cc104404aa1b678ae43e53ac529",
+    "url": "/static/media/futbol.33518cc1.jpg"
+  },
+  {
+    "revision": "acfd85aa69419c598d71a0576cb9df98",
+    "url": "/static/media/gym.acfd85aa.jpeg"
+  },
+  {
+    "revision": "f9ac6d86a294edd81adb24bdcdb78a4f",
+    "url": "/static/media/gym.f9ac6d86.png"
+  },
+  {
+    "revision": "00e93fdfb6da9c1e4c4b6d040082a25d",
+    "url": "/static/media/icofont.00e93fdf.svg"
+  },
+  {
+    "revision": "4abc402d95b5c15108481f94b28e02b2",
+    "url": "/static/media/icofont.4abc402d.woff"
+  },
+  {
+    "revision": "529c3ac7a75675f5a9493f12421e8755",
+    "url": "/static/media/icofont.529c3ac7.ttf"
+  },
+  {
+    "revision": "ea6b50ee81ded0716b21280e0db415fb",
+    "url": "/static/media/icofont.ea6b50ee.eot"
+  },
+  {
+    "revision": "083a1d6f072d91a2a13c283bcea68879",
+    "url": "/static/media/image-sportman.083a1d6f.jpg"
+  },
+  {
+    "revision": "d481019b7d98a9d693296751f2b141d0",
+    "url": "/static/media/karate.d481019b.jpg"
+  },
+  {
+    "revision": "c0992dc64d2c427dead0922e601d427f",
+    "url": "/static/media/logo2.c0992dc6.jpg"
+  },
+  {
+    "revision": "be9c8630ae0281a53aac8a170b6fdf0e",
+    "url": "/static/media/natacion.be9c8630.jpg"
+  },
+  {
+    "revision": "e05b824006cd4c43d2cb204891bf9150",
+    "url": "/static/media/parque.e05b8240.jpg"
+  },
+  {
+    "revision": "509e95440ebee199f1e26fd1fba6bce5",
+    "url": "/static/media/party.509e9544.jpeg"
+  },
+  {
+    "revision": "35855c4d4231be8ac781b80211b98614",
+    "url": "/static/media/piscina.35855c4d.jpg"
+  },
+  {
+    "revision": "f2c2eb14855f31c66d59f1b4aa5db5c4",
+    "url": "/static/media/restaurant.f2c2eb14.jpg"
+  },
+  {
+    "revision": "205bf493598eacfc1fcafe2270d75707",
+    "url": "/static/media/salon-de-fiesta.205bf493.webp"
+  },
+  {
+    "revision": "9171997436a567b13a1e4984ff0636c1",
+    "url": "/static/media/sauna.91719974.jpeg"
+  },
+  {
+    "revision": "0452f8943a3988ab63c929680abb288c",
+    "url": "/static/media/soccer.0452f894.jpg"
+  },
+  {
+    "revision": "208745090fccd545232e8c6bc0e17a5b",
+    "url": "/static/media/spa.20874509.jpeg"
+  },
+  {
+    "revision": "cd3fa500bc3d00b9dff8f1d2036dccf2",
+    "url": "/static/media/spa.cd3fa500.jpg"
+  },
+  {
+    "revision": "ee4a1026314a382f0d3474a77b209c76",
+    "url": "/static/media/swimming.ee4a1026.jpeg"
+  },
+  {
+    "revision": "1ede2398f2dbc935a941561f278ca046",
+    "url": "/static/media/teatro.1ede2398.jpg"
+  },
+  {
+    "revision": "e4b52053e17bdde26c1a71ae07d0b8aa",
+    "url": "/static/media/tenis.e4b52053.jpg"
+  },
+  {
+    "revision": "1f3ffa70fd7e8781700f5176a9bba539",
+    "url": "/static/media/volley.1f3ffa70.jpg"
+  },
+  {
+    "revision": "5b37d56882417fa9e038f627041792ea",
+    "url": "/static/media/volley.5b37d568.jpg"
+  }
+]);
